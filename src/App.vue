35636d715d3c0214973a78e8c076cca3aa36a973
Sebastian Deppisch <git@sebastiandeppisch.de>
2024-12-08 15:29:37 +0100

    move page to page component

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,15 @@
 <script lang="ts" setup>
-import Simulation from './components/Simulation.vue';
-import { getCurrentInstance, ref } from 'vue';
-//import tourSteps from '@/tourConfig';
-import { VTour } from '@globalhive/vuejs-tour';
-import '@globalhive/vuejs-tour/dist/style.css';
-import tourSteps from '@/tourConfig';
-import { faHouse, faQuestion } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-const tour = ref(); 
-
-function startTour () {
-  localStorage.removeItem('vjt-default');
-  tour.value.startTour();
-}
-
-const softwareBuildDate = BUILD_DATE;
-const formattedBuildDate = new Date(softwareBuildDate).toLocaleDateString('de-DE');
-const key = ref(0);
-
-
-function restart () {
-  key.value++;
-}
-
+import SimulationPage from './Pages/SimulationPage.vue';
 </script>
 <template>
-  <VTour ref="tour" :steps="tourSteps" autostart :buttonLabels='{next: "Weiter", back: "Zurück", done: "Beenden", skip: "Überspringen"}'/>
   <div class="min-h-screen bg-gray-100 flex flex-col">
     <header class="bg-gray-800 text-white p-4 shadow-md" data-tour="start">
       <h1 class="text-3xl font-bold text-center">Heizungssteuerung Simulation</h1>
     </header>
 
-    
-
     <main class="flex-1 flex items-center justify-center">
-      <div class="w-full max-w-5xl p-6 ">
-
-        <div class="flex flex-row justify-end mb-4 gap-4 items-center">
-
-          <p class="text-sm text-gray-600 mr-auto">Letzte Änderung: {{ formattedBuildDate }}</p>
-
-          <button @click="restart" class="border border-gray-500 rounded font-bold bg-white p-2" date-tour="new-house"><font-awesome-icon :icon="faHouse" class="pr-2"/>Neues Haus erstellen</button>
-          <button @click="startTour" class="border border-gray-500 rounded font-bold bg-white p-2"><font-awesome-icon :icon="faQuestion" class="pr-2"/>Tour starten</button>
-
-        </div>
-        
-        <Simulation data-tour="test" :key="key"/>
-
+      <div class="w-full max-w-5xl p-6">
+        <SimulationPage />
       </div>
     </main>
 
@@ -57,7 +20,6 @@ function restart () {
 </template>
 
 <style scoped>
-/* Optional: Anpassungen für eine bessere Darstellung */
 body {
   font-family: 'Arial', sans-serif;
 }
---
+++ src/Pages/SimulationPage.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import Simulation from './../components/Simulation.vue';
+import { ref } from 'vue';
+import { VTour } from '@globalhive/vuejs-tour';
+import '@globalhive/vuejs-tour/dist/style.css';
+import tourSteps from '@/tourConfig';
+import { faHouse, faQuestion } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+const tour = ref();
+
+function startTour() {
+	localStorage.removeItem('vjt-default');
+	tour.value.startTour();
+}
+
+const softwareBuildDate = BUILD_DATE;
+const formattedBuildDate = new Date(softwareBuildDate).toLocaleDateString('de-DE');
+const key = ref(0);
+
+
+function restart() {
+	key.value++;
+}
+</script>
+<template>
+	<div>
+		<VTour ref="tour" :steps="tourSteps" autostart
+			:buttonLabels='{ next: "Weiter", back: "Zurück", done: "Beenden", skip: "Überspringen" }' />
+		<div class="flex flex-row justify-end mb-4 gap-4 items-center">
+			<p class="text-sm text-gray-600 mr-auto">Letzte Änderung: {{ formattedBuildDate }}</p>
+			<button @click="restart" class="border border-gray-500 rounded font-bold bg-white p-2"
+				date-tour="new-house"><font-awesome-icon :icon="faHouse" class="pr-2" />Neues Haus erstellen</button>
+			<button @click="startTour" class="border border-gray-500 rounded font-bold bg-white p-2"><font-awesome-icon
+					:icon="faQuestion" class="pr-2" />Tour starten</button>
+		</div>
+		<Simulation data-tour="test" :key="key" />
+	</div>
+</template>
